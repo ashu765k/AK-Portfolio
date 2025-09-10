a8795cd66d3e27abb4d1791962c33b53c03965cb
ashu765k <142171324+ashu765k@users.noreply.github.com>
2025-09-11 00:41:17 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,12 +4,12 @@ import About from '@/components/About';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
-import StarryCursor from '@/components/StarryCursor';
+// import StarryCursor from '@/components/StarryCursor';
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <StarryCursor />
+{/*       <StarryCursor /> */}
       <Navbar />
       
       <main>
